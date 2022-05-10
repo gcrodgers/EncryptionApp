120cacdf4e79db8a01fcfb20e514f0483886e392
Garrett Rodgers <gcrodgers@wisc.edu>
2022-05-10 15:39:18 -0500

    Stage changes and icon

--- EncryptionApp/src/application/GUI.java
+++ EncryptionApp/src/application/GUI.java
@@ -2,19 +2,30 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 
 
 public class GUI extends Application {
+	
+	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			Group root = new Group();
+			Scene scene = new Scene(root, Color.AQUA);
+			stage.setTitle("Ecryption Selecter Demo");
+			stage.setHeight(600);
+			stage.setWidth(600);
+			stage.setResizable(false);
+			Image myIcon = new Image("encryption-icon.png");
+			stage.getIcons().add(myIcon);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			stage.setScene(scene);
+			stage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
