740f915e7ce4c2bfe0ebd3f24d47410290fb499f
Garrett Rodgers <gcrodgers@wisc.edu>
2022-05-12 14:52:01 -0500

    Scene control

---
+++ EncryptionApp/src/application/SwitchScene.java
@@ -0,0 +1,68 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SwitchScene {
+
+	private Parent root;
+	private Stage stage;
+	private Scene scene;
+	
+	public void mainScene(ActionEvent e) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("builder.fxml"));
+		stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void caesarScene(ActionEvent e) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("CaesarScene.fxml"));
+		stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void RSAScene(ActionEvent e) throws IOException {
+		/*
+		root = FXMLLoader.load(getClass().getResource("builder.fxml"));
+		stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	   */
+	}
+	
+	public void DHAScene(ActionEvent e) throws IOException {
+		/*
+		root = FXMLLoader.load(getClass().getResource("builder.fxml"));
+		stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	   */
+	}
+	
+	public void AESScene(ActionEvent e) throws IOException {
+		/*
+		root = FXMLLoader.load(getClass().getResource("builder.fxml"));
+		stage = (Stage) ((Node)e.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	   */
+	}
+	
+	public void exit(ActionEvent e) throws IOException {
+		Platform.exit();
+	}
+}
